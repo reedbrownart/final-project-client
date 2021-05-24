fc15be54557058c91e2645337b1f6215c908ba8c
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-05-24 17:21:46 -0400

    heroku bugs

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3030';
         break;
-    case 'gif-gallery-server.herokuapp.com' :
-        APIURL = 'https://gif-gallery-server.herokuapp.com'
+    case 'final-project-client.herokuapp.com' :
+        APIURL = 'https://final-project-client.herokuapp.com'
 }
 
 export default APIURL;
